c727ffd3ecdb25a50d688f891223e6db770d0b87
pdtieubinh <tinybottles2112@gmail.com>
2023-05-29 09:21:29 +0300

    client pg

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -3,6 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const pool = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
     user: 'lexppvljwytmxz',
     password: process.env.DB_PASSWORD,
     host: 'ec2-52-209-225-31.eu-west-1.compute.amazonaws.com',
