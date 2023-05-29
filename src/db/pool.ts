b11805ef8acb1f390ae249d7829ca91705265efe
pdtieubinh <tinybottles2112@gmail.com>
2023-05-29 09:16:36 +0300

    cookie session

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,13 +1,21 @@
-import { Pool } from 'pg'
+import { Pool, Client } from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const pool = new Pool({
+const pool = new Client({
     user: 'lexppvljwytmxz',
     password: process.env.DB_PASSWORD,
     host: 'ec2-52-209-225-31.eu-west-1.compute.amazonaws.com',
     port: 5432,
     database: 'deju3dm4r6hpjh',
+})
+
+pool.connect((err) => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
 })
 
 export default pool
